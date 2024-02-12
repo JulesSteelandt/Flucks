c4ad10dad484ff0f0d3c63eff3983af0fac4f63e
NicoRiri <nicolate.bernardet@gmail.com>
2024-02-12 12:14:36 +0100

    feat: retour api

--- server/server.js
+++ server/server.js
@@ -3,3 +3,7 @@ import app from './app.js';
 app.listen(process.env.PORT, () => {
   console.log(`🦉 Serveur prêt sur le port ${process.env.PORT}`);
 })
+
+app.use("/routes", apiRouter)
+
+app.get("/", (req, res) => res.send("Hello depuis la racine"))
