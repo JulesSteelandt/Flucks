47339c6fce3324d214282cbece01e96f102c5a2e
Nicolas Bernardet <57874200+NicoRiri@users.noreply.github.com>
2024-02-12 13:56:17 +0100

    feat: repertoire static pour get les vidéos (#4)

--- server/src/app.js
+++ server/src/app.js
@@ -8,6 +8,8 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+app.use(express.static('public'))
+
 app.get("/", (req, res) => res.send("Hello depuis la racine"))
 app.use("/api", apiRouter)
 
