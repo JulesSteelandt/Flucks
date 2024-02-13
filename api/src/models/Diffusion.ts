3eb5491f857d62ef0223d41377ec6b6bfa040332
Jules <sonij590833@gmail.com>
2024-02-13 18:14:52 +0100

    refactor : ajout des tags

--- api/src/models/Diffusion.ts
+++ api/src/models/Diffusion.ts
@@ -6,6 +6,21 @@ class Diffusion {
       const diffusions = await db("Diffusion")
         .select("Diffusion.*", "Utilisateur.pseudo as createurPseudo")
         .leftJoin("Utilisateur", "Diffusion.createur", "Utilisateur.email");
+
+      const tags = await db("Tag").where(
+        "diffusion_id",
+        "in",
+        diffusions.map((diffusion: any) => diffusion.id),
+      );
+
+      diffusions.forEach((diffusion: any) => {
+        const tagsForDiffusion = tags
+          .filter((tag: any) => tag.diffusion_id === diffusion.id)
+          .map((tag: any) => tag.tag);
+
+        diffusion.tags = tagsForDiffusion.length > 0 ? tagsForDiffusion : null;
+      });
+
       return diffusions;
     } catch (error) {
       console.error("Erreur lors de la récupération des diffusions:", error);
@@ -16,10 +31,44 @@ class Diffusion {
   static async getById(diffusionId: string) {
     try {
       const diffusion = await db("Diffusion")
-        .select("Diffusion.*", "Utilisateur.pseudo as createurPseudo")
+        .select(
+          "Diffusion.*",
+          "Utilisateur.pseudo as createurPseudo",
+          "Utilisateur.email as createurEmail",
+          "Geolocalisation.longitude as longitude",
+          "Geolocalisation.latitude as latitude",
+        )
         .leftJoin("Utilisateur", "Diffusion.createur", "Utilisateur.email")
+        .leftJoin(
+          "Geolocalisation",
+          "Diffusion.geolocalisationId",
+          "Geolocalisation.id",
+        )
         .where("Diffusion.id", diffusionId)
         .first();
+
+      // Sélectionner le nombre de likes
+      const likeCount = await db("Like")
+        .count("id as likeCount")
+        .where("diffusion", diffusionId)
+        .first();
+      diffusion.likeCount = likeCount ? likeCount.likeCount : 0;
+
+      const abonnementCount = await db("Abonnement")
+        .count("abonneur as abonnementCount")
+        .where("abonneur", diffusion.createurEmail)
+        .first();
+      diffusion.abonnementCount = abonnementCount
+        ? abonnementCount.abonnementCount
+        : 0;
+
+      // Sélectionner les commentaires
+      const commentaires = await db("Commentaire")
+        .select("Commentaire.*", "Utilisateur.pseudo as pseudo")
+        .leftJoin("Utilisateur", "Commentaire.utilisateur", "Utilisateur.email")
+        .where("diffusion", diffusionId);
+      diffusion.commentaires = commentaires || [];
+
       return diffusion;
     } catch (error) {
       console.error(
