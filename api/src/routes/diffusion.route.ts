de236f4b34893322572891d97ad7428efd5b5225
Jules <sonij590833@gmail.com>
2024-02-13 17:03:23 +0100

    refactor : route diffusionbyid

--- api/src/routes/diffusion.route.ts
+++ api/src/routes/diffusion.route.ts
@@ -1,11 +1,15 @@
 // user.routes.ts
 
 import express from "express";
-import { getDiffusion } from "../controllers/diffusion.controller";
+import {
+  getDiffusion,
+  getDiffusionById,
+} from "../controllers/diffusion.controller";
 
 const router = express.Router();
 
 // Route pour rechercher un utilisateur par e-mail
 router.get("/", getDiffusion);
+router.get("/:id", getDiffusionById);
 
 export default router; // Exportez le routeur par défaut
