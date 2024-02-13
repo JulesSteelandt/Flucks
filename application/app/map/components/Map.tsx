fc743a3574bc980b090d13493a7c2629450ee83f
Nathan <nathan.melbeck@gmail.com>
2024-02-13 13:58:44 +0100

    feat: composant map

--- application/app/map/components/Map.tsx
+++ application/app/map/components/Map.tsx
@@ -1,20 +1,22 @@
-import {MapContainer, Marker, Popup, TileLayer, Tooltip} from 'react-leaflet';
+
+'use client';
+import {MapContainer, Marker, Popup, TileLayer, useMap} from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import 'leaflet-defaulticon-compatibility';
-import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
 
-export default function MyMap(props: any) {
-  const { position, zoom } = props;
 
-  return <MapContainer center={position} zoom={zoom} scrollWheelZoom={false}>
+export default function MyMap() {
+  return (
+
+  <MapContainer className={'w-screen h-[40vw]'} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} >
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
     />
-    <Marker position={position}>
+    <Marker position={[51.505, -0.09]}>
       <Popup>
         A pretty CSS3 popup. <br /> Easily customizable.
       </Popup>
     </Marker>
-  </MapContainer>;
+  </MapContainer>
+  );  
 }
