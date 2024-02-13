4a4858c60cfea33e85178ffac002bd0c18c7c452
Nathan <nathan.melbeck@gmail.com>
2024-02-13 13:59:55 +0100

    feat: page carte

---
+++ application/app/map/page.tsx
@@ -0,0 +1,14 @@
+import Map from './components/Map';
+export default function MapEvent() {
+  return (
+
+    <div>
+
+      <Map ></Map>
+      <h1 className={'text-2xl text-amber-400'}>Map</h1>
+      <p>Test</p>
+
+    </div>
+
+  );
+}
