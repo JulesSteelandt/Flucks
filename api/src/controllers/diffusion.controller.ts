b2676edf53a00dc1bf757eb581738afd0e0d092b
Jules <sonij590833@gmail.com>
2024-02-13 16:50:09 +0100

    feat: affichage de la liste des diffusions

--- api/src/controllers/diffusion.controller.ts
+++ api/src/controllers/diffusion.controller.ts
@@ -4,7 +4,14 @@ import Diffusion from "../models/Diffusion";
 export const getDiffusion = async (req: Request, res: Response) => {
   try {
     const diff = await Diffusion.getAllDiffusions();
-    return res.status(200).json({ diff });
+    console.log();
+    const elementsARetourner = diff.map((element: any) => ({
+      id: element.id,
+      titre: element.titre,
+      createur: element.createurPseudo,
+      direct: element.direct,
+    }));
+    return res.status(200).json({ data: elementsARetourner });
   } catch (error) {
     console.error("Erreur lors du retour des diffusions:", error);
     return res.status(500).json({
--- api/src/models/Diffusion.ts
+++ api/src/models/Diffusion.ts
@@ -3,7 +3,9 @@ const db = require("../config/db");
 class Diffusion {
   static async getAllDiffusions() {
     try {
-      const diffusions = await db("Diffusion").select();
+      const diffusions = await db("Diffusion")
+        .select("Diffusion.*", "Utilisateur.pseudo as createurPseudo")
+        .leftJoin("Utilisateur", "Diffusion.createur", "Utilisateur.email");
       return diffusions;
     } catch (error) {
       console.error("Erreur lors de la récupération des diffusions:", error);
