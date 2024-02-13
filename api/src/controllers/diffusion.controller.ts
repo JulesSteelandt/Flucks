377a282f8464724663f8f20070208f25660e43b5
Jules <sonij590833@gmail.com>
2024-02-13 17:03:39 +0100

    feat : action diffusionbyid

--- api/src/controllers/diffusion.controller.ts
+++ api/src/controllers/diffusion.controller.ts
@@ -10,12 +10,36 @@ export const getDiffusion = async (req: Request, res: Response) => {
       titre: element.titre,
       createur: element.createurPseudo,
       direct: element.direct,
+      urgence: element.urgence,
     }));
     return res.status(200).json({ data: elementsARetourner });
   } catch (error) {
     console.error("Erreur lors du retour des diffusions:", error);
     return res.status(500).json({
       message: "Erreur lors du retour des diffusions:",
+    });
+  }
+};
+
+export const getDiffusionById = async (req: Request, res: Response) => {
+  try {
+    const diffusionId = req.params.id;
+
+    const diffusion = await Diffusion.getById(diffusionId);
+
+    if (!diffusion) {
+      return res.status(404).json({ message: "Diffusion non trouvée." });
+    }
+
+    // Retourner la diffusion
+    return res.status(200).json({ diffusion });
+  } catch (error) {
+    console.error(
+      "Erreur lors de la récupération de la diffusion par ID:",
+      error,
+    );
+    return res.status(500).json({
+      message: "Erreur lors de la récupération de la diffusion par ID.",
     });
   }
 };
--- api/src/models/Diffusion.ts
+++ api/src/models/Diffusion.ts
@@ -12,6 +12,23 @@ class Diffusion {
       throw error;
     }
   }
+
+  static async getById(diffusionId: string) {
+    try {
+      const diffusion = await db("Diffusion")
+        .select("Diffusion.*", "Utilisateur.pseudo as createurPseudo")
+        .leftJoin("Utilisateur", "Diffusion.createur", "Utilisateur.email")
+        .where("Diffusion.id", diffusionId)
+        .first();
+      return diffusion;
+    } catch (error) {
+      console.error(
+        "Erreur lors de la récupération de la diffusion par ID",
+        error,
+      );
+      throw error;
+    }
+  }
 }
 
 export default Diffusion;
