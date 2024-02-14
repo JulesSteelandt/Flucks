e52ee366e1b50816a11b106c425bb7068ff960b2
Nicolas Bernardet <57874200+NicoRiri@users.noreply.github.com>
2024-02-14 15:26:32 +0100

    Backend/webrtc/feat/stream (#9)
    * ignore : ajout des temp et des vidéos finals
    
    * fix: plus besoin de ça, ça fera le dossier automatiquement
    
    * package: up
    
    * feat: ajout des constantes des dossiers
    
    * action lié au socket
    
    * passage sur serveur http

--- server/src/wssHandle/index.js
+++ server/src/wssHandle/index.js
@@ -1,5 +1,34 @@
+import init from "./init.js";
+import stream from "./stream.js";
+import record from "./record.js";
+import end from "./end.js";
+
 export default (wss) => {
     wss.on("connection", (socket) => {
         console.log("Connection")
+
+        let user = {}
+        let file = {}
+
+        socket.on("message", (message) => {
+            console.log(message)
+        })
+
+        socket.on("init", (event) => {
+            init(event, user, file)
+        })
+
+
+        socket.on("stream", (event) => {
+            stream(socket)
+        })
+
+        socket.on("record", (event) => {
+            record(event, user, file)
+        })
+
+        socket.on("disconnect", (event) => {
+            end(socket, user, file)
+        })
     })
 }
