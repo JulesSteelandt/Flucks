4de0723471f6767ad8cd20520a226cd5d11a6771
Nathan <nathan.melbeck@gmail.com>
2024-02-15 12:56:00 +0100

    fix: mauvaise quote fixed...

--- application/app/utils/appGlobal.ts
+++ application/app/utils/appGlobal.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = 'http://docketu.iutnc.univ-lorraine.fr:35305';
+export const API_BASE_URL : string = 'http://docketu.iutnc.univ-lorraine.fr:35305';
 
-export const API_GEOLOCALISATION = '${API_BASE_URL}/geolocalisation';
+export const API_GEOLOCALISATION : string = `${API_BASE_URL}/geolocalisation`;
