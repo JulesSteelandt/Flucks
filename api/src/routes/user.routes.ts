764983cfeeca4d09914adcd7c70546f160df0bc7
Jules <sonij590833@gmail.com>
2024-02-15 17:10:38 +0100

    feat: route follow

--- api/src/routes/user.routes.ts
+++ api/src/routes/user.routes.ts
@@ -2,11 +2,12 @@
 
 import express from "express";
 import { signIn, signUp } from "../controllers/user.controller";
+import { checkToken } from "../middlewares/checkToken";
 
 const router = express.Router();
 
 router.post("/signin", signIn);
 router.post("/signup", signUp);
-router.post("/follow", signUp);
+router.post("/follow", checkToken, follow);
 
 export default router;
