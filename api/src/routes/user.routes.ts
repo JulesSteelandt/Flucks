d58fb2596d8486a30076359679574e57f0ca600e
Jules <sonij590833@gmail.com>
2024-02-15 17:10:21 +0100

    feat: route follow

--- api/src/routes/user.routes.ts
+++ api/src/routes/user.routes.ts
@@ -5,8 +5,8 @@ import { signIn, signUp } from "../controllers/user.controller";
 
 const router = express.Router();
 
-// Route pour rechercher un utilisateur par e-mail
 router.post("/signin", signIn);
 router.post("/signup", signUp);
+router.post("/follow", signUp);
 
-export default router; // Exportez le routeur par défaut
+export default router;
