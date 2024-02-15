de9e953c9f94fcedaac57bb2dd2a193dbed3a0ee
Nathan <nathan.melbeck@gmail.com>
2024-02-15 11:01:58 +0100

    feat: bouton refresh pour actualiser la carte avec le css associé

--- application/app/map/page.tsx
+++ application/app/map/page.tsx
@@ -4,18 +4,27 @@ import Map from './components/Map';
 
 export default function MapEvent() {
 
-  const [testReact, setTestReact] = useState('ok');
+  // state
+
+  // comportement
+  const reloadPage = () => {
+    window.location.reload();
+  };
 
 
+  // affichage
   return (
+    <div className={'bg-[#E0E2E8] w-5/6'}>
+      <div className={'h-full  flex flex-col m-10'}>
 
-    <div>
-
-      <Map></Map>
-      <h1 className={'text-2xl text-amber-400'}>Map</h1>
-      <p>Test</p>
-      <label>test react : {testReact} </label>
-
+        <Map></Map>
+        <div className={'bg-[#D9D9D9] flex flex-col sm:flex-row justify-between mt-10 p-6'}>
+          <h1 className={'text-xl font-bold mb-2 sm:mb-0'}>Les évènements proches de vous.</h1>
+          <button className={'bg-[#19AFFB] hover:bg-blue-400 text-white font-bold py-2 px-4 rounded-xl'}
+                  onClick={reloadPage}>Rafraichir
+          </button>
+        </div>
+      </div>
     </div>
 
   );
