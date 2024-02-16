094e2f1d0593ccf88a8951e9694768a4117c3832
NathanMelbeck <94992898+NathanMelbeck@users.noreply.github.com>
2024-02-16 17:43:59 +0100

    fix: css composant CheckboxLinear.tsx

--- application/app/flucks/stream/create/components/CheckboxLinear.tsx
+++ application/app/flucks/stream/create/components/CheckboxLinear.tsx
@@ -16,7 +16,7 @@ const Switcher1 = () => {
                     onChange={handleCheckboxChange}
                     className='sr-only'
                 />
-                <div className={` block h-8 w-14 rounded-full bg-[#E5E7EB] ${isChecked ? 'bg-[#10B981]' : 'bg-[#BFC1C6]'}`}></div>
+                <div className={` block h-8 w-14 rounded-full ${isChecked ? 'bg-[#10B981]' : 'bg-[#BFC1C6]'}`}></div>
                 <div className={`drop-shadow-lg dot absolute left-1 top-1 h-6 w-6 rounded-full bg-white transition ${isChecked ? 'transform translate-x-6' : ''}`}></div>
             </div>
         </label>
