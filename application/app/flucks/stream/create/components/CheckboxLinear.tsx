b25387725e6a2029a0f8b2203444b245765c30c3
NathanMelbeck <94992898+NathanMelbeck@users.noreply.github.com>
2024-02-16 13:55:56 +0100

    fix: image path + css

--- application/app/flucks/stream/create/components/CheckboxLinear.tsx
+++ application/app/flucks/stream/create/components/CheckboxLinear.tsx
@@ -16,8 +16,8 @@ const Switcher1 = () => {
                     onChange={handleCheckboxChange}
                     className='sr-only'
                 />
-                <div className={`block h-8 w-14 rounded-full bg-[#E5E7EB] ${isChecked ? 'bg-[#10B981]' : ''}`}></div>
-                <div className={`dot absolute left-1 top-1 h-6 w-6 rounded-full bg-white transition ${isChecked ? 'transform translate-x-6' : ''}`}></div>
+                <div className={` block h-8 w-14 rounded-full bg-[#E5E7EB] ${isChecked ? 'bg-[#10B981]' : 'bg-[#BFC1C6]'}`}></div>
+                <div className={`drop-shadow-lg dot absolute left-1 top-1 h-6 w-6 rounded-full bg-white transition ${isChecked ? 'transform translate-x-6' : ''}`}></div>
             </div>
         </label>
     );
--- application/app/flucks/stream/create/page.tsx
+++ application/app/flucks/stream/create/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import {useState} from 'react';
-import CheckBoxLinear from './components/CheckboxLinear';
 import CheckboxLinear from './components/CheckboxLinear';
+import Image from 'next/image';
 
 
 export default function CreateStream() {
@@ -13,33 +13,42 @@ export default function CreateStream() {
 
     // affichage
     return (
-        <div className={'bg-[#E0E2E8] w-5/6'}>
-            <div className={'h-full  flex flex-col m-10'}>
+        <section className={'bg-[#E0E2E8] w-5/6'}>
+            <div className={'h-auto w-auto flex flex-col m-10'}>
 
 
-                <div className={'flex justify-around'}>
-                    <div className={'flex flex-col'}>
+                <div className={'flex justify-center'}>
+                    <div className={'flex flex-col w-full'}>
                         <label>Titre du stream :</label>
-                        <textarea></textarea>
+                        <textarea className={'rounded-lg drop-shadow-lg resize-none'}></textarea>
 
                         <label>Description du stream :</label>
-                        <textarea></textarea>
+                        <textarea className={'rounded-lg drop-shadow-lg resize-none'}></textarea>
 
                     </div>
 
-                    <div className={'flex flex-col'}>
+                    <div className={'border-black border-[1px] mx-2'}>
+                    </div>
+
+                    <div className={'flex flex-col gap-2 w-full'}>
                         <label>Tags :</label>
-                        <textarea></textarea>
+                        <textarea className={'rounded-lg drop-shadow-lg resize-none'}></textarea>
 
-                        <div>
+                        <div className={'flex bg-white rounded-lg p-1 justify-center items-center drop-shadow-lg max-w-[250px] gap-5'}>
                             <CheckboxLinear/>
-
+                            <label className={'font-semibold mr-2'}>Géolocalisation</label>
                         </div>
+
+                        <div className={'flex self-center'}>
+                            <Image src={'/../img/FlecheDouble.png'} width={30} height={30} alt={'fleche'} />
+                            <p className={'bg-[#19AFFB] py-1 px-2 rounded-lg text-white'} >Lancer la diffusion en direct</p>
+                        </div>
+
                     </div>
 
                 </div>
             </div>
-        </div>
+        </section>
 
     );
 }
