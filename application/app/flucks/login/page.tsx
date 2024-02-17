fb387fa42b4cd495a8f7ee3a990c0bf065b6a2a0
NathanMelbeck <94992898+NathanMelbeck@users.noreply.github.com>
2024-02-17 16:43:40 +0100

    fix: <div> devient un <form/> avec des submit

--- application/app/flucks/login/page.tsx
+++ application/app/flucks/login/page.tsx
@@ -2,7 +2,8 @@ import Link from 'next/link';
 
 export default function Page() {
   return (
-    <div className={'flex w-5/6 flex-col items-center'}>
+    <form className={'flex w-5/6 flex-col items-center'}>
+
       <p className={'p-6 text-3xl font-bold'}>Inscription</p>
       <img src={'/img/desktop_logo.png'} className={'w-1/5'} />
       <div className={'mb-4 w-1/4'}>
@@ -14,13 +15,13 @@ export default function Page() {
         <input type={'text'} className={'w-full rounded-lg border-2 p-1 drop-shadow-lg'} />
       </div>
       <div className={'flex flex-row'}>
-        <button
+        <input type={'submit'} value={'Se connecter'}
           className={
             'drop-shadow-lg mr-4 rounded-xl bg-[#19AFFB] px-6 py-2 text-sm font-bold text-white hover:border-2 hover:bg-white hover:text-[#19AFFB]'
           }
-        >
-          Se connecter
-        </button>
+        />
+
+
         <Link
           className={'rounded-xl border-2 bg-gray-100 px-6 py-2 text-sm font-bold hover:bg-black hover:text-white drop-shadow-lg'}
           href={'flucks/connexion'}
@@ -28,6 +29,6 @@ export default function Page() {
           Je n'ai pas encore de compte
         </Link>
       </div>
-    </div>
+    </form>
   );
 }
