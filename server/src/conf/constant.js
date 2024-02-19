77dfce274cc83250424496939e800e17b9cb0e8c
Nicolas Bernardet <57874200+NicoRiri@users.noreply.github.com>
2024-02-19 15:49:44 +0100

    Backend/webrtc/main (#37)
    * Backend/webrtc/init/express (#1)
    
    * package : express
    
    * on retire ça la
    
    * ignore: ajout de node modules
    
    * init: application express
    
    * init: server
    
    * ignore: .env
    
    * feat: retour api (#3)
    
    * fix: package
    
    * fix: docker
    
    * ignore: node module
    
    * fix
    
    * refactor: log
    
    * feat: repertoire static pour get les vidéos (#4)
    
    * retour simple de wss (#5)
    
    * Backend/webrtc/feat/stream (#9)
    
    * ignore : ajout des temp et des vidéos finals
    
    * fix: plus besoin de ça, ça fera le dossier automatiquement
    
    * package: up
    
    * feat: ajout des constantes des dossiers
    
    * action lié au socket
    
    * passage sur serveur http
    
    * Pas encore testé mais normalement devrait marcher avec un form html et pas postman (#16)
    
    * fix: reddif gérer si l'utilisateur crash
    
    * maquette de l'action
    
    * implémentation de la maquette
    
    * feat: changement d'état de vidéo
    
    * fix: uniquement la personne qui a créer la video peut la mettre en privé publique

--- server/src/conf/constant.js
+++ server/src/conf/constant.js
@@ -1,2 +1,3 @@
 export const DOSSIER_TEMPORAIRE = "./public/temp"
 export const DOSSIER_VIDEO = "./public/video"
+export const DOSSIER_PRIVE = "./private/"
--- server/src/routes/apiRouter.js
+++ server/src/routes/apiRouter.js
@@ -1,6 +1,7 @@
 import express from "express"
 import {helloWorldAction} from '../actions/helloWorldAction.js';
 import {uploadVideoAction} from '../actions/uploadVideoAction.js';
+import {patchVisibiliteAction} from "../actions/patchVisibiliteAction.js";
 
 const router = express.Router()
 
@@ -13,5 +14,9 @@ router
 .route("/upload")
     .post((req, res, next) => uploadVideoAction(req, res, next))
     .all((req, res, next) => next(405))
+
+router
+    .route("/video/visibilite")
+    .patch((req, res, next) => patchVisibiliteAction(req, res, next))
 
 export default router
