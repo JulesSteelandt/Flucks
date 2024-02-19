3f825da7f6a0b78fcf7bbaf91a01edadd2813dc7
Nathan <nathan.melbeck@gmail.com>
2024-02-19 10:50:38 +0100

    fix: css

--- application/app/flucks/login/page.tsx
+++ application/app/flucks/login/page.tsx
@@ -5,25 +5,26 @@ export default function Page() {
     <form className={'flex w-5/6 flex-col items-center'}>
 
       <p className={'p-6 text-3xl font-bold'}>Inscription</p>
-      <img src={'/img/desktop_logo.png'} className={'w-1/5'} />
-      <div className={'mb-4 w-1/4'}>
+      <img src={'/img/desktop_logo.png'} className={'w-1/5 min-w-[150px]'} />
+      <div className={'mb-4 w-fit mx-auto'}>
         <p className={'mb-1 text-sm font-bold'}>Email :</p>
-        <input type={'text'} className={'w-full rounded-lg border-2 p-1 drop-shadow-lg'} />
+        <input type={'text'} className={'w-full rounded-lg border-2 p-1 drop-shadow-lg min-w-[250px]'} />
       </div>
-      <div className={'mb-4 w-1/4'}>
+      <div className={'mb-4 w-fit mx-auto'}>
         <p className={'mb-1 text-sm font-bold'}>Mot de passe :</p>
-        <input type={'text'} className={'w-full rounded-lg border-2 p-1 drop-shadow-lg'} />
+        <input type={'text'} className={'w-full rounded-lg border-2 p-1 drop-shadow-lg min-w-[250px]'} />
       </div>
-      <div className={'flex flex-row'}>
+
+      <div className={'flex flex-col  md:flex-row gap-2'}>
         <input type={'submit'} value={'Se connecter'}
-          className={
-            'drop-shadow-lg mr-4 rounded-xl bg-[#19AFFB] px-6 py-2 text-sm font-bold text-white hover:border-2 hover:bg-white hover:text-[#19AFFB]'
-          }
+               className={
+                 'drop-shadow-lg mr-4 rounded-xl bg-[#19AFFB] px-6 py-2 text-sm font-bold text-white hover:border-2 hover:bg-white hover:text-[#19AFFB] min-w-fit'
+               }
         />
 
 
         <Link
-          className={'rounded-xl border-2 bg-gray-100 px-6 py-2 text-sm font-bold hover:bg-black hover:text-white drop-shadow-lg'}
+          className={'rounded-xl border-2 bg-gray-100 px-6 py-2 text-sm font-bold hover:bg-black hover:text-white drop-shadow-lg min-w-fit'}
           href={'flucks/connexion'}
         >
           Je n'ai pas encore de compte
