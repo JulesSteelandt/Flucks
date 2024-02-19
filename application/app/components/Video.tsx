f937aeeb010fa60863a39828a72eb8ffab2b765b
NathanMelbeck <94992898+NathanMelbeck@users.noreply.github.com>
2024-02-19 15:08:09 +0100

    Frontend/pages (#35)
    * feat: page user + css
    
    * fix: css gap-2
    
    * feat: page creation video créer et css
    
    * feat: css et creation component
    
    * feat: image button play de la maquette
    
    * refractor: move to @/components
    
    * fix: fix typescripte
    
    * feat: composant VideoWaiting.tsx pour les video en attente de publication

--- application/app/components/Video.tsx
+++ application/app/components/Video.tsx
@@ -1,18 +1,20 @@
-'use client'
+'use client';
+import React from 'react';
+import {useRouter} from 'next/navigation';
 
-import {useRouter} from "next/navigation";
-
-export default function Video({ title, creator, id }) {
+export default function Video({ title, creator, id }: { title: string; creator: string; id: string }) {
     const router = useRouter();
     return (
-        <div id="carte" className={'border-2 rounded-xl bg-[#5DA5B3] w-1/3 m-2'}>
-            <div className={'flex justify-center py-8'} onClick={() => router.push('/flucks/video/' + id)}>
-                <img src={"/img/play_icon.png"} alt={"play logo"} className={'w-12'}/>
+        <div id='carte' className={'border-2 rounded-xl bg-[#5DA5B3] m-2'}>
+            <div className={'flex justify-center py-8'} onClick={() => {
+return router.push(`/flucks/video/${  id}`);
+}}>
+                <img src={'/img/video_play_img.png'} alt={'play logo'} className={'w-12'}/>
             </div>
             <div className={'flex flex-row justify-between bg-white rounded-b-xl p-2'}>
                 <p className={'text-sm font-bold'}>{title}</p>
                 <p className={'text-sm italic'}>{creator}</p>
             </div>
         </div>
-    )
+    );
 }
