a789862eaf770e24309527486d2ce922a5f2e440
Jules <sonij590833@gmail.com>
2024-02-19 16:46:52 +0100

    refactor: redirection

--- application/app/components/Header.tsx
+++ application/app/components/Header.tsx
@@ -1,23 +1,33 @@
-'use client'
+'use client';
 
-import React from "react";
-import Link from "next/link";
-import {useRouter} from "next/navigation";
+import React from 'react';
+import Link from 'next/link';
+import {useRouter} from 'next/navigation';
 
 export default function Header() {
+  const router = useRouter();
 
-    const router = useRouter();
-
-    return (
-        <header className={'flex flex-row justify-between bg-[#5DA5B3] p-4'}>
-            <img src={'/img/flucks_logo.png'} alt={'logo flucks'} className={'w-72'} onClick={() => router.push('/flucks')}/>
-            <div className={'flex flex-row justify-center items-center'}>
-                <div className={'flex flex-col mr-4'}>
-                    <Link href={'/flucks/inscription'} className={'bg-white font-bold rounded-2xl p-1 my-1 flex justify-center'}>S'inscrire</Link>
-                    <Link href={'/flucks/connexion'} className={'bg-white font-bold rounded-2xl p-1 my-1 flex justify-center'}>Se connecter</Link>
-                </div>
-                <img src={'/img/flucks_profile.png'} alt={'flucks profile'} className={'rounded-t-full rounded-br-full w-28'}/>
-            </div>
-        </header>
-    )
+  return (
+    <header className={'flex flex-row justify-between bg-[#5DA5B3] p-4'}>
+      <img
+        src={'/img/flucks_logo.png'}
+        alt={'logo flucks'}
+        className={'w-72'}
+        onClick={() => {
+          return router.push('/flucks');
+        }}
+      />
+      <div className={'flex flex-row items-center justify-center'}>
+        <div className={'mr-4 flex flex-col'}>
+          <Link href={'/flucks/inscription'} className={'my-1 flex justify-center rounded-2xl bg-white p-1 font-bold'}>
+            S'inscrire
+          </Link>
+          <Link href={'/flucks/login'} className={'my-1 flex justify-center rounded-2xl bg-white p-1 font-bold'}>
+            Se connecter
+          </Link>
+        </div>
+        <img src={'/img/flucks_profile.png'} alt={'flucks profile'} className={'w-28 rounded-t-full rounded-br-full'} />
+      </div>
+    </header>
+  );
 }
--- application/app/flucks/login/page.tsx
+++ application/app/flucks/login/page.tsx
@@ -3,29 +3,31 @@ import Link from 'next/link';
 export default function Page() {
   return (
     <form className={'flex w-full flex-col items-center md:w-5/6'}>
-
       <p className={'p-6 text-3xl font-bold'}>Inscription</p>
       <img src={'/img/desktop_logo.png'} className={'w-1/5 min-w-[150px]'} />
-      <div className={'mb-4 w-fit mx-auto'}>
+      <div className={'mx-auto mb-4 w-fit'}>
         <p className={'mb-1 text-sm font-bold'}>Email :</p>
-        <input type={'text'} className={'w-full rounded-lg border-2 p-1 drop-shadow-lg min-w-[250px]'} />
+        <input type={'text'} className={'w-full min-w-[250px] rounded-lg border-2 p-1 drop-shadow-lg'} />
       </div>
-      <div className={'mb-4 w-fit mx-auto'}>
+      <div className={'mx-auto mb-4 w-fit'}>
         <p className={'mb-1 text-sm font-bold'}>Mot de passe :</p>
-        <input type={'text'} className={'w-full rounded-lg border-2 p-1 drop-shadow-lg min-w-[250px]'} />
+        <input type={'text'} className={'w-full min-w-[250px] rounded-lg border-2 p-1 drop-shadow-lg'} />
       </div>
 
-      <div className={'flex flex-col  md:flex-row gap-2'}>
-        <input type={'submit'} value={'Se connecter'}
-               className={
-                 'drop-shadow-lg mr-4 rounded-xl bg-[#19AFFB] px-6 py-2 text-sm font-bold text-white hover:border-2 hover:bg-white hover:text-[#19AFFB] min-w-fit'
-               }
+      <div className={'flex flex-col  gap-2 md:flex-row'}>
+        <input
+          type={'submit'}
+          value={'Se connecter'}
+          className={
+            'mr-4 min-w-fit rounded-xl bg-[#19AFFB] px-6 py-2 text-sm font-bold text-white drop-shadow-lg hover:border-2 hover:bg-white hover:text-[#19AFFB]'
+          }
         />
 
-
         <Link
-          className={'rounded-xl border-2 bg-gray-100 px-6 py-2 text-sm font-bold hover:bg-black hover:text-white drop-shadow-lg min-w-fit'}
-          href={'flucks/connexion'}
+          className={
+            'min-w-fit rounded-xl border-2 bg-gray-100 px-6 py-2 text-sm font-bold drop-shadow-lg hover:bg-black hover:text-white'
+          }
+          href={'inscription'}
         >
           Je n'ai pas encore de compte
         </Link>
