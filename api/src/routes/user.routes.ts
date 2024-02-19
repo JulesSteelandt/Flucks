b475ade1d23de372bd540a581947c9f5d647ad2e
Jules <sonij590833@gmail.com>
2024-02-19 13:37:30 +0100

    feat: acceder a une vidéo précise si la vidéo est en privée et que l'utilisateur est le bon

--- api/src/routes/user.routes.ts
+++ api/src/routes/user.routes.ts
@@ -7,8 +7,11 @@ import {
   signIn,
   signUp,
   video,
+  videoById,
 } from "../controllers/user.controller";
 import { checkToken } from "../middlewares/checkToken";
+import { checkDiffusionCreateur } from "../middlewares/checkDiffusionCreateur";
+import { checkDiffusionIdExist } from "../middlewares/checkDiffusionIdExist";
 
 const router = express.Router();
 
@@ -16,6 +19,13 @@ router.post("/signin", signIn);
 router.post("/signup", signUp);
 router.post("/follow", checkToken, follow);
 router.get("/video", checkToken, video);
+router.get(
+  "/video/:id",
+  checkToken,
+  checkDiffusionIdExist,
+  checkDiffusionCreateur,
+  videoById,
+);
 router.get("/abonnement", checkToken, getAbonnements);
 
 export default router;
