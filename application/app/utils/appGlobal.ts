c9f5c8cec689b5bbecb331bbc336e4b7e156cdc9
Nathan <nathan.melbeck@gmail.com>
2024-02-19 16:14:47 +0100

    feat: route creation d'un stream

--- application/app/utils/appGlobal.ts
+++ application/app/utils/appGlobal.ts
@@ -3,3 +3,5 @@ export const API_BASE_URL : string = 'http://docketu.iutnc.univ-lorraine.fr:3530
 export const API_DIFFUSIONS : string = 'http://docketu.iutnc.univ-lorraine.fr:35305';
 
 export const API_GEOLOCALISATION : string = `${API_BASE_URL}/geolocalisation`;
+
+export const API_CREATE_STREAM : string = `${API_BASE_URL}/diffusions/create`;
