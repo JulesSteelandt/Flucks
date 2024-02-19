27519a797e8a5be1b115eaa995776eb831d0f573
NicoRiri <nicolate.bernardet@gmail.com>
2024-02-19 15:17:12 +0100

    fix: uniquement la personne qui a créer la video peut la mettre en privé publique

--- server/src/actions/patchVisibiliteAction.js
+++ server/src/actions/patchVisibiliteAction.js
@@ -1,8 +1,8 @@
 import {existsSync, mkdirSync, rename} from "fs"
 import {DOSSIER_PRIVE, DOSSIER_VIDEO} from "../conf/constant.js";
+import { default as axios } from 'axios';
 
-export function patchVisibiliteAction(req, res, next) {
-    try {
+export async function patchVisibiliteAction(req, res, next) {
         if (!existsSync(DOSSIER_PRIVE)) {
             mkdirSync(DOSSIER_PRIVE, {recursive: true});
         }
@@ -10,44 +10,54 @@ export function patchVisibiliteAction(req, res, next) {
         const PublicPath = `${DOSSIER_VIDEO}/${req.body.id}.mp4`
         const PrivatePath = `${DOSSIER_PRIVE}/${req.body.id}.mp4`
 
-        if (req.body.public === 1) {
-
-            if (!existsSync(PublicPath)) {
-                if (existsSync(PrivatePath)) {
-                    rename(PrivatePath, PublicPath, (err) => {
-                        if (err) throw err
-                        console.log(`${req.body.id} est devenu publique`)
-                        return res.status(200).json({message: 'La vidéo est devenu publique'});
-                    })
-                } else {
-                    return res.status(400).json({message: 'La vidéo n\'existe pas dans le répertoire privé'});
-                }
-            } else {
-                return res.status(400).json({message: 'La vidéo est déjà publique'});
+        if(!req.headers.authorization){
+            return res.status(400).json({message: 'Pas de credentials envoyés'});
+        } else {
+            const token = req.headers.authorization.split(' ')[1];
+            try {
+                const response = await axios.get(`http://docketu.iutnc.univ-lorraine.fr:35305/users/video/${req.body.id}`, {
+                    headers: {
+                        "Authorization": `Bearer ${token}`
+                    }
+                })
+            } catch (e) {
+                return res.status(400).json({message: e.response.data.message});
             }
 
 
-        } else if (req.body.public === 0) {
-            if (existsSync(PublicPath)) {
-                if (!existsSync(PrivatePath)) {
-                    rename(PublicPath, PrivatePath, (err) => {
-                        if (err) throw err
-                        console.log(`${req.body.id} est devenu privé`)
-                        return res.status(200).json({message: 'La vidéo est devenu privée'});
+            if (req.body.public === 1) {
 
-                    })
+                if (!existsSync(PublicPath)) {
+                    if (existsSync(PrivatePath)) {
+                        rename(PrivatePath, PublicPath, (err) => {
+                            if (err) throw err
+                            console.log(`${req.body.id} est devenu publique`)
+                            return res.status(200).json({message: 'La vidéo est devenu publique'});
+                        })
+                    } else {
+                        return res.status(400).json({message: 'La vidéo n\'existe pas dans le répertoire privé'});
+                    }
                 } else {
-                    return res.status(400).json({message: 'La vidéo n\'existe pas dans le répertoire publique'});
+                    return res.status(400).json({message: 'La vidéo est déjà publique'});
                 }
-            } else {
-                return res.status(400).json({message: 'La vidéo est déjà privée'});
-            }
 
+
+            } else if (req.body.public === 0) {
+                if (existsSync(PublicPath)) {
+                    if (!existsSync(PrivatePath)) {
+                        rename(PublicPath, PrivatePath, (err) => {
+                            if (err) throw err
+                            console.log(`${req.body.id} est devenu privé`)
+                            return res.status(200).json({message: 'La vidéo est devenu privée'});
+
+                        })
+                    } else {
+                        return res.status(400).json({message: 'La vidéo n\'existe pas dans le répertoire publique'});
+                    }
+                } else {
+                    return res.status(400).json({message: 'La vidéo est déjà privée'});
+                }
+
+            }
         }
-
-
-    } catch (e) {
-        console.error(e)
-        next(500)
-    }
 }
