f200e15f0ad74478a415fa7342aea49d3f651f6d
Jules <97917784+JulesSteelandt@users.noreply.github.com>
2024-02-20 10:50:00 +0100

    Frontend/page/login (#38)
    * feat: creation login (non responsive)
    
    * feat: ajout de react-auth-kit
    
    * feat: ajout de  @auth-kit/next
    
    * fix: changement pour next-auth
    
    * fix: <div> devient un <form/> avec des submit
    
    * fix: css
    
    * fix: css pour menu tel
    
    * refactor: redirection
    
    * refactor: package.json
    
    * feat: login et cookie
    
    * feat: getCookieToken et getDecodedToken
    
    * refactor: console.log en trop
    
    * test commit
    
    * test commit
    
    * feat: redirection
    
    ---------
    
    Co-authored-by: NathanMelbeck <94992898+NathanMelbeck@users.noreply.github.com>
    Co-authored-by: Nathan <nathan.melbeck@gmail.com>
    Co-authored-by: Sonii08~ <sonij590833@gmail.com~>

--- application/app/components/Video.tsx
+++ application/app/components/Video.tsx
@@ -1,26 +1,23 @@
-'use client'
+'use client';
 
-import {useRouter, usePathname} from "next/navigation";
-import Link from "next/link";
+import {useRouter} from 'next/navigation';
 
-export default function Video({ title, creator, id }: { title: string; creator: string; id: string }) {
-    const router = useRouter();
-    const pathNameTest = usePathname();
-
-    return (
-        <div className={'border-2 rounded-xl bg-[#5DA5B3] xl:w-1/4 max-xl:w-1/3 max-md:w-1/2 relative'}>
-            <div className={'flex justify-center py-8'} onClick={() => router.push('/flucks/video/' + id)}>
-                <img src={"/img/play_icon.png"} alt={"play logo"} className={'w-12'}/>
-            </div>
-            {(pathNameTest === "/flucks/my_videos") && (
-                <Link href={'/flucks/edit/' + id} className={'m-2 absolute right-0 top-0'}>
-                    <img src={'/img/pencil_icon.png'} alt={'edit'} width={25}/>
-                </Link>
-            )}
-            <div className={'flex flex-row justify-between bg-white rounded-b-xl p-2'}>
-                <p className={'text-sm font-bold'}>{title}</p>
-                <p className={'text-sm italic'}>{creator}</p>
-            </div>
-        </div>
-    )
+export default function Video({title, creator, id}) {
+  const router = useRouter();
+  return (
+    <div id='carte' className={'m-2 w-1/3 rounded-xl border-2 bg-[#5DA5B3]'}>
+      <div
+        className={'flex justify-center py-8'}
+        onClick={() => {
+          return router.push(`/flucks/video/${id}`);
+        }}
+      >
+        <img src={'/img/play_icon.png'} alt={'play logo'} className={'w-12'} />
+      </div>
+      <div className={'flex flex-row justify-between rounded-b-xl bg-white p-2'}>
+        <p className={'text-sm font-bold'}>{title}</p>
+        <p className={'text-sm italic'}>{creator}</p>
+      </div>
+    </div>
+  );
 }
