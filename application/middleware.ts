67bfc5fa9a9390d943eb1615add62fc543ac5e52
Jules <sonij590833@gmail.com>
2024-02-20 13:40:27 +0100

    reafctor: ajout d'une route

--- application/middleware.ts
+++ application/middleware.ts
@@ -8,5 +8,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/flucks/stream/create', '/flucks/my_videos'],
+  matcher: ['/flucks/stream/create', '/flucks/my_videos', '/flucks/user'],
 };
