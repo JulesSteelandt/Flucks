9cdd568d6e072bf0ff12d362c942bf73c9a1b45f
Hergott <emilien.herg@gmail.com>
2024-02-21 10:11:45 +0100

    modifs css

--- application/app/flucks/user/page.tsx
+++ application/app/flucks/user/page.tsx
@@ -15,7 +15,7 @@ export default function CreateStream() {
         }).catch(error => {
             console.log(error);
         })
-    },[]);
+    }, []);
 
     return (
         <section className={'bg-[#E0E2E8] w-full md:w-5/6'}>
@@ -30,35 +30,33 @@ export default function CreateStream() {
 
 
                         <form className={'flex flex-col gap-2'}>
-                            <label className={'font-semibold'}>Email :</label>
-                            <p>{userEmail}</p>
-                            <input type={'text'} className={'rounded-lg drop-shadow-lg resize-none'}/>
+                            <label className={''}><strong>Email :</strong> {userEmail}</label>
+                            <input type={'text'}
+                                   className={'w-full rounded-lg border-2 p-1 drop-shadow-lg min-w-[250px] resize-none'}/>
 
-                            <label className={'font-semibold'}>Pseudo :</label>
-                            <p>{userPseudo}</p>
-                            <input type={'text'} className={'rounded-lg drop-shadow-lg resize-none'}/>
+                            <label className={''}><strong>Pseudo :</strong> {userPseudo}</label>
+                            <input type={'text'}
+                                   className={'w-full rounded-lg border-2 p-1 drop-shadow-lg min-w-[250px] resize-none'}/>
 
                             <input type={'submit'}
-                                   className={'flex self-end bg-[#19AFFB] py-1 px-2 rounded-lg text-white drop-shadow-lg'}
+                                   className={'flex self-end bg-[#19AFFB] py-2 px-6 rounded-xl font-bold text-sm text-white drop-shadow-lg hover:border-2 hover:bg-white hover:text-[#19AFFB]'}
                                    value={'Enregistrer'}/>
                         </form>
                     </div>
-
-
                     <div className={'border-black border-[1px] mx-8 hidden md:block'}></div>
-
-
                     <form className={'flex flex-col gap-2 md:w-1/3'}>
                         <label className={'self-center font-semibold'}>Changer de mot de passe</label>
 
                         <label>Nouveau mot de passe :</label>
-                        <input type={'text'} className={'rounded-lg drop-shadow-lg resize-none'}/>
+                        <input type={'text'}
+                               className={'w-full rounded-lg border-2 p-1 drop-shadow-lg min-w-[250px] resize-none'}/>
 
                         <label>Confirmation du mot de passe :</label>
-                        <input type={'text'} className={'rounded-lg drop-shadow-lg resize-none'}/>
+                        <input type={'text'}
+                               className={'w-full rounded-lg border-2 p-1 drop-shadow-lg min-w-[250px] resize-none'}/>
 
                         <input type={'submit'}
-                               className={'flex self-end bg-[#19AFFB] py-1 px-2 rounded-lg text-white drop-shadow-lg'}
+                               className={'flex self-end bg-[#19AFFB] py-2 px-6 rounded-xl font-bold text-sm text-white drop-shadow-lg hover:border-2 hover:bg-white hover:text-[#19AFFB]'}
                                value={'Enregistrer'}/>
 
                     </form>
