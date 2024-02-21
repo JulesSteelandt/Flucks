a0d7a3dd334681d01ccdaeb918cd597141d22ca7
Jules <sonij590833@gmail.com>
2024-02-21 17:05:09 +0100

    merge

--- application/app/utils/appGlobal.ts
+++ application/app/utils/appGlobal.ts
@@ -1,18 +1,18 @@
-export const API_BASE_URL : string = 'http://docketu.iutnc.univ-lorraine.fr:35305';
+export const API_BASE_URL: string = 'https://flucks-api.timeuh.fr';
 
-export const API_WS_URL : string = 'http://docketu.iutnc.univ-lorraine.fr:35303';
+export const API_WS_URL: string = 'https://flucks-live.timeuh.fr';
 
-export const API_DIFFUSIONS : string = `${API_BASE_URL}/diffusions`;
+export const API_DIFFUSIONS: string = `${API_BASE_URL}/diffusions`;
 
-export const API_GEOLOCALISATION : string = `${API_BASE_URL}/geolocalisation`;
+export const API_GEOLOCALISATION: string = `${API_BASE_URL}/geolocalisation`;
 
-export const API_CREATE_STREAM : string = `${API_BASE_URL}/diffusions/create`;
+export const API_CREATE_STREAM: string = `${API_BASE_URL}/diffusions/create`;
 
-export const API_WS_STREAM : string = `${API_WS_URL}/`;
+export const API_WS_STREAM: string = `${API_WS_URL}/`;
 
 export const APi_SIGNIN: string = `${API_BASE_URL}/users/signin`;
 
 export const API_STOP_STREAM: string = `${API_BASE_URL}/diffusions/stop`;
-export const API_VIDEO_USER : string = `${API_BASE_URL}/users/video`;
+export const API_VIDEO_USER: string = `${API_BASE_URL}/users/video`;
 
-export const API_ABONNEMENTS : string = `${API_BASE_URL}/users/abonnement`;
+export const API_ABONNEMENTS: string = `${API_BASE_URL}/users/abonnement`;
