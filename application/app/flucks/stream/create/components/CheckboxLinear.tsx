3c0a6adfd201a54cc574b9640329cf328182856d
NathanMelbeck <94992898+NathanMelbeck@users.noreply.github.com>
2024-02-21 15:32:12 +0100

    Frontend/diffusion direct (#48)
    * feat: debut page création d'un stream
    
    * fix: composant CheckboxLinear.tsx
    
    * fix: image path + css
    
    * feat: responsive
    
    * feat: creation composant pour voir la preview du stream
    
    * feat: preview stream camera (son composant)
    
    * feat: route creation d'un stream
    
    * feat: creation d'un stream et redirect vers le stream une fois créé
    
    * feat: preparation rtc
    
    * feat: stream fonctionel coté host
    
    * feat: stream fonctionel coté host avec un id par défaut
    
    * fix: fix id streamHost
    
    * feat: viwer stream fonctionnel
    
    * feat: viewer watch automatically the stream
    
    * feat: merge and routage bouton dans le menu
    
    * fix: répare le merge et fix les stream et viewer
    
    * feat: redirection à l'accueil une fois le stream terminé
    
    * feat: token récupérer lors de la creation d'un stream
    
    * feat: fin de stream patch ok
    
    * feat: choix description et titre. Les tag et géolocalisation à revoir

--- application/app/flucks/stream/create/components/CheckboxLinear.tsx
+++ application/app/flucks/stream/create/components/CheckboxLinear.tsx
@@ -1,26 +1,27 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
-const Switcher1 = () => {
-    const [isChecked, setIsChecked] = useState(false);
+type Props = {
+  onClick: () => void;
+}
 
-    const handleCheckboxChange = () => {
-        setIsChecked(!isChecked);
-    };
+const Switcher1 = ({onClick}: Props) => {
+  const [isChecked, setIsChecked] = useState(false);
 
-    return (
-        <label className='flex cursor-pointer select-none items-center'>
-            <div className='relative'>
-                <input
-                    type='checkbox'
-                    checked={isChecked}
-                    onChange={handleCheckboxChange}
-                    className='sr-only'
-                />
-                <div className={` block h-8 w-14 rounded-full bg-[#E5E7EB] ${isChecked ? 'bg-[#10B981]' : 'bg-[#BFC1C6]'}`}></div>
-                <div className={`drop-shadow-lg dot absolute left-1 top-1 h-6 w-6 rounded-full bg-white transition ${isChecked ? 'transform translate-x-6' : ''}`}></div>
-            </div>
-        </label>
-    );
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
+
+  return (
+    <label className='flex cursor-pointer select-none items-center' onClick={onClick}>
+      <div className='relative'>
+        <input type='checkbox' checked={isChecked} onChange={handleCheckboxChange} className='sr-only' />
+        <div className={` block h-8 w-14 rounded-full ${isChecked ? 'bg-[#10B981]' : 'bg-[#BFC1C6]'}`}></div>
+        <div
+          className={`dot absolute left-1 top-1 h-6 w-6 rounded-full bg-white drop-shadow-lg transition ${isChecked ? 'translate-x-6 transform' : ''}`}
+        ></div>
+      </div>
+    </label>
+  );
 };
 
 export default Switcher1;
