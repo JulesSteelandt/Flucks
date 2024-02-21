8221e6b72ab65cd260563fbeb2e72655905220b4
Jules <sonij590833@gmail.com>
2024-02-21 12:01:34 +0100

    refactor: route api, et signup

--- application/app/utils/appGlobal.ts
+++ application/app/utils/appGlobal.ts
@@ -4,4 +4,6 @@ export const API_GEOLOCALISATION: string = `${API_BASE_URL}/geolocalisation`;
 
 export const API_DIFFUSIONS: string = 'http://docketu.iutnc.univ-lorraine.fr:35305';
 
-export const APi_SIGNIN: string = `${API_BASE_URL}/users/signin`;
+export const API_SIGNIN: string = `${API_BASE_URL}/users/signin`;
+
+export const API_SIGNUP: string = `${API_BASE_URL}/users/signup`;
