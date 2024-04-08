2997fa1ca70cb0dcd4f3cc55f76094eaadd23a27
Jules <sonij590833@gmail.com>
2024-04-08 15:28:32 +0200

    fix: config

--- application/next.config.js
+++ application/next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = {
-  output: 'standalone',
+const nextConfig = {
+  // Existing configurations
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'docketu.iutnc.univ-lorraine.fr',
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
+
+module.exports = nextConfig;
