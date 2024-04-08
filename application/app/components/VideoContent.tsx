79dac7cb50a6590f9d91c660ef4d451c71beeba3
Jules <sonij590833@gmail.com>
2024-04-08 15:28:59 +0200

    fix: type

--- application/app/components/VideoContent.tsx
+++ application/app/components/VideoContent.tsx
@@ -1,25 +1,21 @@
-import {useState} from "react";
-import classNames from "classnames";
+import {useState} from 'react';
+import classNames from 'classnames';
 
-export default function Page(id) {
+export default function Page(id: any) {
+  const [videoNotFound, setVideoNotFound] = useState(false);
+  const handleVideoError = () => {
+    setVideoNotFound(true);
+  };
 
-    const [videoNotFound, setVideoNotFound] = useState(false);
-    const handleVideoError = () => {
-        setVideoNotFound(true);
-    }
-
-    return (
-        <div className={classNames('bg-black w-[calc(100% - 32px)] h-2/3 flex justify-center items-center mb-4')}>
-            {
-                videoNotFound ? (
-                    <p className={'text-white'}>Erreur de chargement de la vidéo</p>
-                ) : (
-                    <video className={'h-full'} controls onError={handleVideoError}>
-                        <source src={"https://docketu.iutnc.univ-lorraine.fr:35303/video/" + id.id + ".mp4"}
-                                type={"video/mp4"}/>
-                    </video>
-                )
-            }
-        </div>
-    )
+  return (
+    <div className={classNames('w-[calc(100% - 32px)] mb-4 flex h-2/3 items-center justify-center bg-black')}>
+      {videoNotFound ? (
+        <p className={'text-white'}>Erreur de chargement de la vidéo</p>
+      ) : (
+        <video className={'h-full'} controls onError={handleVideoError}>
+          <source src={'https://docketu.iutnc.univ-lorraine.fr:35303/video/' + id.id + '.mp4'} type={'video/mp4'} />
+        </video>
+      )}
+    </div>
+  );
 }
--- application/app/flucks/stream/[id]/host/page.tsx
+++ application/app/flucks/stream/[id]/host/page.tsx
@@ -1,47 +1,41 @@
-import {fetchDiffusionDataWithID} from '@/app/data';
 import React from 'react';
 import StreamerHost from '@/app/flucks/stream/[id]/host/components/StreamerComponent';
 import {API_DIFFUSIONS} from '@/app/utils/appGlobal';
 
-export default async function Page({params}: {
-    params: {
-        id: string
-    }
+export default async function Page({
+  params,
+}: {
+  params: {
+    id: string;
+  };
 }) {
   const fetchStreamDataWithID = async () => {
     try {
-      const res = await fetch(`${API_DIFFUSIONS  }/${  params.id}`, {cache: 'no-cache'});
+      const res = await fetch(`${API_DIFFUSIONS}/${params.id}`, {cache: 'no-cache'});
       if (!res.ok) {
         console.error('Erreur de récupération des marqueurs');
         return;
       }
       return await res.json();
     } catch (e) {
-        console.log('erreur lors de la création du stream');
+      console.log('erreur lors de la création du stream');
     }
   };
 
-  let streamData = {};
+  let streamData: any = {};
   try {
-     streamData = await fetchStreamDataWithID();
+    streamData = await fetchStreamDataWithID();
     console.log(streamData);
-    if (streamData === undefined)    streamData = {data: {titre: 'inconnu' }};
-
+    if (streamData === undefined) streamData = {data: {titre: 'inconnu'}};
   } catch (e) {
     console.log('erreur');
   }
 
-    return (
-        <div className={'p-4 w-5/6 m-8'}>
-          <StreamerHost id={params.id} />
-                <p className={'bg-[#D9D9D9] w-5/6 text-center p-4 font-bold'}>{streamData.data.titre} </p>
-            <div className={'flex flex-row justify-between'}>
-
-            </div>
-
-
-
-        </div>
-
-    );
+  return (
+    <div className={'m-8 w-5/6 p-4'}>
+      <StreamerHost id={params.id} />
+      <p className={'w-5/6 bg-[#D9D9D9] p-4 text-center font-bold'}>{streamData.data.titre} </p>
+      <div className={'flex flex-row justify-between'}></div>
+    </div>
+  );
 }
