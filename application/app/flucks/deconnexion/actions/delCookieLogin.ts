0d5d281a08badf5cdfe77970b3541ed1fe6ba5b1
Jules <sonij590833@gmail.com>
2024-04-09 12:49:25 +0200

    fix : cookie client

--- application/app/flucks/deconnexion/actions/delCookieLogin.ts
+++
@@ -1,8 +0,0 @@
-'use server';
-import {cookies} from 'next/headers';
-
-const delCookieLogin = async () => {
-  cookies().delete('token');
-};
-
-export default delCookieLogin;
--- application/app/flucks/login/actions/setCookieLogin.ts
+++
@@ -1,8 +0,0 @@
-'use server';
-import {cookies} from 'next/headers';
-
-const setCookieLogin = async (token: string) => {
-  cookies().set('token', token);
-};
-
-export default setCookieLogin;
