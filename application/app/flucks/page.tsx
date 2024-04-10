94d6327aaa4166e57b932295e9280e2fddedd1b2
Hergott <emilien.herg@gmail.com>
2024-04-10 16:12:31 +0200

    ajout icon +

--- application/app/flucks/page.tsx
+++ application/app/flucks/page.tsx
@@ -8,16 +8,20 @@ export default function Page() {
             <div className={'border-b-2 border-gray-200 pb-8'}>
                 <div className={'flex flex-row justify-between items-center w-full'}>
                     <p className={'p-8 text-2xl font-bold'}>Les derniers streams</p>
-                    <Link href={'/flucks/streams'} className={'flex justify-center w-1/6 m-4 rounded-xl bg-[#19AFFB] px-6 py-2 text-sm font-bold text-white hover:border-2 hover:bg-white hover:text-[#19AFFB]'}>
-                        <p>Voir plus de streams</p>
+                    <Link href={'/flucks/streams'}
+                          className={'flex justify-center items-center m-4 rounded-xl bg-[#19AFFB] md:px-6 max-md:px-2 md:py-2 max-md:py-2 max-md:py-1 text-sm font-bold text-white hover:border-2 hover:bg-white hover:text-[#19AFFB]'}>
+                        <p className={'max-md:hidden'}>Voir plus de streams</p>
+                        <img className={'md:hidden'} src={'/img/plus_icon.png'} alt={'+'} width={25}/>
                     </Link>
                 </div>
                 <StreamList limit={'8'}/>
             </div>
             <div className={'flex flex-row justify-between items-center w-full'}>
                 <p className={'font-bold p-8 text-2xl'}>Les dernières vidéos</p>
-                <Link href={'/flucks/videos'} className={'flex justify-center w-1/6 m-4 rounded-xl bg-[#19AFFB] px-6 py-2 text-sm font-bold text-white hover:border-2 hover:bg-white hover:text-[#19AFFB]'}>
-                    <p>Voir plus de vidéos</p>
+                <Link href={'/flucks/videos'}
+                      className={'flex justify-center items-center m-4 rounded-xl bg-[#19AFFB] md:px-6 max-md:px-2 md:py-2 max-md:py-2 max-md:py-1 text-sm font-bold text-white hover:border-2 hover:bg-white hover:text-[#19AFFB]'}>
+                    <p className={'max-md:hidden'}>Voir plus de vidéos</p>
+                    <img className={'md:hidden'} src={'/img/plus_icon.png'} alt={'+'} width={25}/>
                 </Link>
             </div>
             <VideoList limit={'8'}/>
